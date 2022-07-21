11bcacb146408a15f92b12eb3b213202b8ee94c4
Henri Nurmi <henri@nurmi.me>
2022-07-21 10:09:24 +0300

    Update nvim-tree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,51 +1,39 @@
 local nvimtree = require("nvim-tree")
 
-local g = vim.g
-
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
---
-g.nvim_tree_show_icons = {
-  folders = 1,
-  -- folder_arrows= 1
-  files = 1,
-  git = 1,
-}
-
-g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    deleted = "",
-    ignored = "◌",
-    renamed = "➜",
-    staged = "✓",
-    unmerged = "",
-    unstaged = "✗",
-    untracked = "★",
-  },
-  folder = {
-  -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-  -- arrow_open = "",
-  -- arrow_closed = "",
-    default = "",
-    empty = "", -- 
-    empty_open = "",
-    open = "",
-    symlink = "",
-    symlink_open = "",
-  },
-}
-
 nvimtree.setup {
   git = {
     enable = false,
   },
   renderer = {
+    root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" },
+    highlight_git = true,
     indent_markers = {
       enable = false,
+    },
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          deleted = "",
+          ignored = "◌",
+          renamed = "➜",
+          staged = "✓",
+          unmerged = "",
+          unstaged = "✗",
+          untracked = "★",
+        },
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+      },
     },
   },
   diagnostics = {
