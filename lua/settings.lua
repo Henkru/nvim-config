5a62017496173d634401a1df2e23e066181977ff
Henri Nurmi <henri@nurmi.me>
2022-12-30 13:06:35 +0200

    Verilog indentation = 2 spaces, and fusesoc's .core files are yaml

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,30 +11,30 @@ opt.swapfile = false
 opt.hidden = true
 
 -- UI --
-opt.relativenumber = true     -- show relative line number
-opt.showmatch = true          -- highlight matching parenthesis
-opt.foldmethod = 'marker'     -- enable folding (default 'foldmarker')
-opt.splitright = true         -- vertical split to the right
-opt.splitbelow = true         -- orizontal split to the bottom
-opt.ignorecase = true         -- ignore case letters when search
-opt.smartcase = true          -- ignore lowercase for the whole pattern
-opt.linebreak = true          -- wrap on word boundary
+opt.relativenumber = true -- show relative line number
+opt.showmatch = true -- highlight matching parenthesis
+opt.foldmethod = 'marker' -- enable folding (default 'foldmarker')
+opt.splitright = true -- vertical split to the right
+opt.splitbelow = true -- orizontal split to the bottom
+opt.ignorecase = true -- ignore case letters when search
+opt.smartcase = true -- ignore lowercase for the whole pattern
+opt.linebreak = true -- wrap on word boundary
 opt.signcolumn = 'yes'
 
 -- Color --
 opt.termguicolors = true
 
 -- Indentation --
-opt.expandtab = true          -- use spaces instead of tabs
-opt.shiftwidth = 4            -- shift 4 spaces when tab
-opt.tabstop = 4               -- 1 tab == 4 spaces
-opt.smartindent = true        -- autoindent new lines
+opt.expandtab = true -- use spaces instead of tabs
+opt.shiftwidth = 4 -- shift 4 spaces when tab
+opt.tabstop = 4 -- 1 tab == 4 spaces
+opt.smartindent = true -- autoindent new lines
 opt.autoindent = true
 -- stylua: ignore end
 
 -- 2 spaces for selected filetypes
 cmd([[
-  autocmd FileType xml,html,xhtml,css,scss,haskell,javascript,lua,yaml,typescript setlocal shiftwidth=2 tabstop=2
+  autocmd FileType xml,html,xhtml,css,scss,haskell,javascript,lua,yaml,typescript,systemverilog,verilog setlocal shiftwidth=2 tabstop=2
 ]])
 
 -- don't auto commenting new lines
@@ -50,4 +50,9 @@ cmd([[
 
 cmd([[
   au BufRead plugins.lua lua require 'user.update'
+]])
+
+-- Fusesoc .core files are yaml
+cmd([[
+  au BufRead,BufNewFile *.core set filetype=yaml
 ]])
