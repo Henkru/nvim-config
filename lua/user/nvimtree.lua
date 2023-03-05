e76e811bd735047e3c9568564ef89c87c94e7de7
Henri Nurmi <henri@nurmi.me>
2023-03-05 19:55:44 +0200

    Update nvim-tree

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -51,7 +51,6 @@ nvimtree.setup({
   },
   disable_netrw = true,
   hijack_netrw = true,
-  ignore_ft_on_setup = { 'dashboard' },
   open_on_tab = false,
   hijack_cursor = true,
   update_cwd = true,
