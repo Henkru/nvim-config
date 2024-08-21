5a1432066f23dddbcd39786c12196d085f2db6c6
Henri Nurmi <henri@nurmi.me>
2024-08-21 08:54:29 +0300

    Add the 'external' folder to telescope's ignore list

--- lua/henkru/telescope.lua
+++ lua/henkru/telescope.lua
@@ -32,7 +32,7 @@ require('telescope').setup({
       preview_cutoff = 120,
     },
     file_sorter = require('telescope.sorters').get_fuzzy_file,
-    file_ignore_patterns = { 'node_modules' },
+    file_ignore_patterns = { 'node_modules', 'external' },
     generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
     path_display = { 'absolute' },
     winblend = 0,
@@ -54,10 +54,10 @@ require('telescope').setup({
   },
   extensions = {
     fzf = {
-      fuzzy = true, -- false will only do exact matching
+      fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = 'smart_case',        -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
     },
     hoogle = {
