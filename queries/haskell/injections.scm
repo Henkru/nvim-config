f732febc24d6a8f8898152331347d057ed0c0828
Henri Nurmi <henri@nurmi.me>
2025-04-03 23:08:48 +0300

    haskell: disable injections

--- queries/haskell/injections.scm
+++ queries/haskell/injections.scm
@@ -1,4 +1,4 @@
-(exp_apply
-    (exp_name (variable) @_fname (#match? @_fname "^(execute|executeMany|query)_?$"))
-    (quasiquote (quasiquote_body) @sql)
-)
+; (exp_apply
+;     (exp_name (variable) @_fname (#match? @_fname "^(execute|executeMany|query)_?$"))
+;     (quasiquote (quasiquote_body) @sql)
+; )
