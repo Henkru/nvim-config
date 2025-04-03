f6bfa94e8bdc1cb7be29cb445c92c49b0352ea2a
Henri Nurmi <henri@nurmi.me>
2025-04-03 22:15:05 +0300

    diagnostic: convert to vim.diagnostic.config

--- lua/henkru/lsp.lua
+++ lua/henkru/lsp.lua
@@ -22,12 +22,32 @@ local diagnostic_custom = {}
 vim.diagnostic.config(diagnostic_default)
 
 -- Change diagnostics symbols
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  signs = vim.g.have_nerd_font and {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  } or {},
+  virtual_text = {
+    source = 'if_many',
+    spacing = 2,
+    format = function(diagnostic)
+      local diagnostic_message = {
+        [vim.diagnostic.severity.ERROR] = diagnostic.message,
+        [vim.diagnostic.severity.WARN] = diagnostic.message,
+        [vim.diagnostic.severity.INFO] = diagnostic.message,
+        [vim.diagnostic.severity.HINT] = diagnostic.message,
+      }
+      return diagnostic_message[diagnostic.severity]
+    end,
+  },
+})
 
 --                           --
 --     LSP Configuration     --
